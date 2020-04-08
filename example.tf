6ba42f7daa234896e88eecc09070bac9d9822082
1Mill <erik.ekberg@outlook.com>
2020-04-08 16:27:48 -0700

    Add route to shared credentials

--- example.tf
+++ example.tf
@@ -1,10 +1,11 @@
 provider "aws" {
-	profile		= "default"
-	region 		= "us-east-1"
-	version 	= "~> 2.56"
+	profile			= "terraform"
+	region 			= "us-east-1"
+	shared_credentials_file	= "/root/.aws"
+	version 		= "~> 2.56"
 }
 
 resource "aws_instance" "example" {
-	ami		= "ami-2757f631"
-	instance_type 	= "t2.micro"
+	ami			= "ami-2757f631"
+	instance_type 		= "t2.micro"
 }
