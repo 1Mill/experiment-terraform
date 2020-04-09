0dba71da30dee1f499ca1c12c48638436415d3fb
1Mill <erik.ekberg@outlook.com>
2020-04-09 16:49:42 -0700

    Update example.tf

--- example.tf
+++ example.tf
@@ -1,3 +1,15 @@
+terraform {
+	backend "s3" {
+		bucket			= "experiment-terraform-state"
+		dynamodb_table		= "experiment-terraform-state-locks"
+		encrypt			= true
+		key			= "global/s3/terraform.tfstate"
+		profile			= "terraform"
+		region			= "us-east-1"
+		shared_credentials_file	= "/root/.aws"
+	}
+}
+
 provider "aws" {
 	profile			= "terraform"
 	region 			= "us-east-1"
