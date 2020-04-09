a19d36ffb9faff2ed8205377b9defc3c2bceba00
1Mill <erik.ekberg@outlook.com>
2020-04-08 21:05:13 -0700

    Update main.tf

--- state/main.tf
+++ state/main.tf
@@ -5,6 +5,17 @@ provider "aws" {
 	version 		= "~> 2.56"
 }
 
+resource "aws_dynamodb_table" "terraform_locks" {
+	billing_mode	= "PAY_PER_REQUEST"
+	hash_key 	= "LockID"
+	name 		= "experiment-terraform-state-locks"
+
+	attribute {
+		name 	= "LockID"
+		type 	= "S"
+	}
+}
+
 resource "aws_s3_bucket" "terraform_state" {
 	bucket = "experiment-terraform-state"
 
