216db429948017fae75454ace2068150af28f457
1Mill <erik.ekberg@outlook.com>
2020-04-10 15:07:36 -0700

    Remove leading space before json policy object

--- example.tf
+++ example.tf
@@ -37,18 +37,18 @@ resource "aws_lambda_function" "example" {
 resource "aws_iam_role" "lambda_exec" {
 	name = "serverless_example_lambda"
 	assume_role_policy = <<EOF
+{
+	"Statement": [
 		{
-			"Statement": [
-				{
-					"Action": "sts:AssumeRole",
-					"Effect": "Allow",
-					"Principal": {
-						"Service": "lambda.amazonaws.com"
-					},
-					"Sid": ""
-				}
-			],
-			"Version": "2012-10-17"
+			"Action": "sts:AssumeRole",
+			"Effect": "Allow",
+			"Principal": {
+				"Service": "lambda.amazonaws.com"
+			},
+			"Sid": ""
 		}
+	],
+	"Version": "2012-10-17"
+}
 	EOF
 }
