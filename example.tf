e7586153721faacb28712b39222416a2ff72d035
1Mill <erik.ekberg@outlook.com>
2020-04-10 15:01:58 -0700

    Scaffold serverless infrastructure

--- example.tf
+++ example.tf
@@ -24,3 +24,31 @@ provider "aws" {
 // 	ami = "ami-b374d5a5"
 // 	instance_type	= "t2.micro"
 // }
+
+resource "aws_lambda_function" "example" {
+	function_name = "ServerlessExample"
+	handler = "index.handler"
+	role = aws_iam_role.lambda_exec.arn
+	runtime = "nodejs12.x"
+	s3_bucket = "experiment-terraform-serverless"
+	s3_key = "v0.0.1/example.zip"
+}
+
+resource "aws_iam_role" "lambda_exec" {
+	name = "serverless_example_lambda"
+	assume_role_policy = <<EOF
+		{
+			"Statement": [
+				{
+					"Action": "sts:AssumeRole",
+					"Effect": "Allow",
+					"Principal": {
+						"Service": "lambda.amazonaws.com"
+					},
+					"Sid": ""
+				}
+			],
+			"Version": "2012-10-17"
+		}
+	EOF
+}
