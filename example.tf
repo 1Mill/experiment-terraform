95a766fdba8c40fcc61fcf6a989b18b52931ba9f
1Mill <erik.ekberg@outlook.com>
2020-04-09 21:38:30 -0700

    Formatting

--- example.tf
+++ example.tf
@@ -2,25 +2,25 @@ terraform {
 	required_version = "~> 0.12.24"
 
 	backend "s3" {
-		bucket			= "experiment-terraform-state"
-		dynamodb_table		= "experiment-terraform-state-locks"
-		encrypt			= true
-		key			= "global/s3/terraform.tfstate"
-		profile			= "terraform"
-		region			= "us-east-1"
-		shared_credentials_file	= "/root/.aws"
+		bucket = "experiment-terraform-state"
+		dynamodb_table = "experiment-terraform-state-locks"
+		encrypt = true
+		key = "global/s3/terraform.tfstate"
+		profile = "terraform"
+		region = "us-east-1"
+		shared_credentials_file = "/root/.aws"
 	}
 }
 
 provider "aws" {
-	profile			= "terraform"
-	region 			= "us-east-1"
-	shared_credentials_file	= "/root/.aws"
-	version 		= "~> 2.56"
+	profile = "terraform"
+	region = "us-east-1"
+	shared_credentials_file = "/root/.aws"
+	version = "~> 2.56"
 }
 
 resource "aws_instance" "example" {
-	// ami		= "ami-2757f631"
-	ami		= "ami-b374d5a5"
+	// ami = "ami-2757f631"
+	ami = "ami-b374d5a5"
 	instance_type	= "t2.micro"
 }
