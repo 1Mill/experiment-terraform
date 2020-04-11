76b7c6b67303d6165b8743298b006bdcff38df7f
1Mill <erik.ekberg@outlook.com>
2020-04-10 17:01:23 -0700

    Create example_serverless_integration.tf

---
+++ example_serverless_integration.tf
@@ -0,0 +1,27 @@
+resource "aws_api_gateway_integration" "lambda" {
+	http_method = aws_api_aws_api_gateway_method.proxy.http_method
+	resource_id = aws_api_gateway_method.proxy.resource_id
+	rest_api_id = aws_api_gateway_rest_api.example.id
+
+	integration_http_method = "POST"
+	type = "AWS_PROXY"
+	uri = aws_lambda_function.example.invoke_arn
+}
+
+resource "aws_api_gateway_integration" "lambda_root" {
+	http_method = aws_api_gateway_method.proxy_root.http_method
+	resource_id = aws_api_aws_api_gateway_method.proxy_root.resource_id
+	rest_api_id = aws_api_gateway_rest_api.example.id
+
+	integration_http_method = "POST"
+	type = "AWS_PROXY"
+	uri = aws_lambda_function.example.invoke_arn
+}
+
+resource "aws_api_gateway_method" "proxy_root" {
+	resource_id = aws_api_gateway_rest_api.root_resource_id
+	rest_api_id = aws_api_getway_rest_api.example.id
+
+	authorization = "NONE"
+	http_method = "ANY"
+}
