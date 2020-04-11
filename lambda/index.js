d913674850727e8a2d64b6f97c2d681caeab1aa7
1Mill <erik.ekberg@outlook.com>
2020-04-10 17:22:47 -0700

    Update index.js

--- lambda/index.js
+++ lambda/index.js
@@ -2,7 +2,8 @@
 
 exports.handler = (_event, _context, callback) => {
 	const response = {
-		body: '<p>Hello world!</p>',
+		// body: '<p>Hello world!</p>',
+		body: '<p>Bonjour au monde!</p>',
 		headers: { 'Content-Type': 'text/html; charset=utf-8' },
 		statusCode: 200,
 	};
