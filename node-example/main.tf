5e3cae0c3bf7d84cdabb51b6cce97f9c75330edc
1Mill <erik.ekberg@outlook.com>
2020-04-12 13:28:06 -0700

    Update main.tf

--- node-example/main.tf
+++ node-example/main.tf
@@ -26,3 +26,33 @@ resource "aws_ecr_repository" "experiment_node_example" {
 resource "aws_ecs_cluster" "experiment_node_cluster" {
   name = "experiment_node_cluster"
 }
+
+resource "aws_ecs_task_definition" "expeirment_node_task" {
+	family = "experiment_node_task"
+	container_definitions = <<DEF
+[
+	{
+		"cpu": 256,
+		"essential": true,
+		"image": "${aws_ecr_repository.experiment_node_example.repository_url}",
+		"memory": 512,
+		"name: "experiment_node_task",
+		"portMappings": [
+			{
+				"containerPort": 3000,
+				"hostPort": 3000
+			}
+		]
+	}
+]
+DEF
+	requries_compatabilities = ["FARGATE"]
+	network_mode = "awsvpc"
+	memory = 512
+	cpu = 256
+	execution_role_arn = ${data.aws_iam_role.ecs_task_execution_role.arn}
+}
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+	name = "ecsTaskExecutionRole"
+}
