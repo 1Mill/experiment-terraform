0743cae9ae4fd0212011d7b2e087465ec75f3491
1Mill <erik.ekberg@outlook.com>
2020-04-11 18:56:43 -0700

    Scaffold express app

--- node-example/index.js
+++ node-example/index.js
@@ -1,1 +1,10 @@
-console.log('TESTING');
+const express = require('express');
+const app = express();
+
+app.get('/', (res, req) => {
+	res.send('Hello world!');
+});
+
+app.listen(process.env.PORT, () => {
+	console.log(`Listening on port ${process.env.PORT}`);
+});
