067287532abc4f0e26031376f3e681baa279cb97
1Mill <erik.ekberg@outlook.com>
2020-04-11 19:19:56 -0700

    Create main.tf

---
+++ node-example/main.tf
@@ -0,0 +1,24 @@
+terraform {
+	required_version = "~> 0.12.24"
+
+	backend "s3" {
+		bucket = "experiment-terraform-state"
+		dynamodb_table = "experiment-terraform-state-locks"
+		encrypt = true
+		key = "global/s3/terraform.tfstate"
+		profile = "terraform"
+		region = "us-east-1"
+		shared_credentials_file = "/root/.aws"
+	}
+}
+
+provider "aws" {
+	profile = "terraform"
+	region = "us-east-1"
+	shared_credentials_file = "/root/.aws"
+	version = "~> 2.56"
+}
+
+resource "aws_ecr_repository" "experiment_node_example" {
+	name = "experiment_node_example"
+}
