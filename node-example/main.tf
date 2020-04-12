9fcb18035cabe24fb767ee9e1a1afaf57cc1f04e
1Mill <erik.ekberg@outlook.com>
2020-04-12 14:04:52 -0700

    Add networks

--- node-example/main.tf
+++ node-example/main.tf
@@ -13,6 +13,15 @@ resource "aws_ecs_service" "experiment_node_service" {
 	launch_type = "FARGATE"
 	name = "experiment_node_service"
 	task_definition = aws_ecs_task_definition.expeirment_node_task.arn
+
+	network_configuration {
+		assign_public_ip = true
+		subnets = [
+			aws_default_subnet.default_subnet_a,
+			aws_default_subnet.default_subnet_b,
+			aws_default_subnet.default_subnet_c
+		]
+	}
 }
 
 resource "aws_ecs_task_definition" "expeirment_node_task" {
