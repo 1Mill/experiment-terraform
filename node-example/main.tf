6dbac93c3c7dce67c963c27e41cdf4363f93202b
1Mill <erik.ekberg@outlook.com>
2020-04-12 13:31:32 -0700

    Seperate terraform into seperate files

--- node-example/main.tf
+++ node-example/main.tf
@@ -1,26 +1,6 @@
-terraform {
-	required_version = "~> 0.12.24"
 
-	backend "s3" {
-		bucket = "experiment-terraform-state"
-		dynamodb_table = "experiment-terraform-state-locks"
-		encrypt = true
-		key = "global/s3/terraform.tfstate"
-		profile = "terraform"
-		region = "us-east-1"
-		shared_credentials_file = "/root/.aws"
-	}
-}
-
-provider "aws" {
-	profile = "terraform"
-	region = "us-east-1"
-	shared_credentials_file = "/root/.aws"
-	version = "~> 2.56"
-}
-
-resource "aws_ecr_repository" "experiment_node_example" {
-	name = "experiment_node_example"
+data "aws_iam_role" "ecs_task_execution_role" {
+	name = "ecsTaskExecutionRole"
 }
 
 resource "aws_ecs_cluster" "experiment_node_cluster" {
@@ -28,7 +8,6 @@ resource "aws_ecs_cluster" "experiment_node_cluster" {
 }
 
 resource "aws_ecs_task_definition" "expeirment_node_task" {
-	family = "experiment_node_task"
 	container_definitions = <<DEF
 [
 	{
@@ -46,13 +25,10 @@ resource "aws_ecs_task_definition" "expeirment_node_task" {
 	}
 ]
 DEF
-	requries_compatabilities = ["FARGATE"]
-	network_mode = "awsvpc"
-	memory = 512
 	cpu = 256
 	execution_role_arn = ${data.aws_iam_role.ecs_task_execution_role.arn}
-}
-
-resource "aws_iam_role" "ecs_task_execution_role" {
-	name = "ecsTaskExecutionRole"
+	family = "experiment_node_task"
+	memory = 512
+	network_mode = "awsvpc"
+	requries_compatabilities = ["FARGATE"]
 }
---
+++ node-example/registry.tf
@@ -0,0 +1,3 @@
+resource "aws_ecr_repository" "experiment_node_example" {
+	name = "experiment_node_example"
+}
