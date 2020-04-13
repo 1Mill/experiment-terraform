70352d82466977ab27b94608af277762172e4961
1Mill <erik.ekberg@outlook.com>
2020-04-13 16:59:55 -0700

    Move resources around

--- pipeline/lambda.tf
+++ pipeline/lambda.tf
@@ -1,39 +1,4 @@
-resource "aws_lambda_function" "example" {
-	function_name = "ServerlessExample"
-	handler = "index.handler"
-	role = aws_iam_role.lambda_exec.arn
-	runtime = "nodejs12.x"
-	s3_bucket = "experiment-terraform-serverless"
-	s3_key = "v${var.app_version}/example.zip"
-}
-
-resource "aws_iam_role" "lambda_exec" {
-	name = "serverless_example_lambda"
-	assume_role_policy = <<EOF
-{
-	"Statement": [
-		{
-			"Action": "sts:AssumeRole",
-			"Effect": "Allow",
-			"Principal": {
-				"Service": "lambda.amazonaws.com"
-			},
-			"Sid": ""
-		}
-	],
-	"Version": "2012-10-17"
-}
-	EOF
-}
-
-resource "aws_lambda_permission" "apigw" {
-	action = "lambda:InvokeFunction"
-	function_name = aws_lambda_function.example.function_name
-	principal = "apigateway.amazonaws.com"
-	source_arn = "${aws_api_gateway_rest_api.example.execution_arn}/*/*"
-	statement_id = "AllowAPIGatewayInvoke"
-}
-
+// API Gateway
 resource "aws_api_gateway_integration" "lambda" {
 	http_method = aws_api_gateway_method.proxy.http_method
 	resource_id = aws_api_gateway_method.proxy.resource_id
@@ -90,4 +55,42 @@ resource "aws_api_gateway_method" "proxy" {
 
 	authorization = "NONE"
 	http_method = "ANY"
+}
+
+// Lambda
+resource "aws_lambda_function" "example" {
+	function_name = "ServerlessExample"
+	handler = "index.handler"
+	role = aws_iam_role.lambda_exec.arn
+	runtime = "nodejs12.x"
+	s3_bucket = "experiment-terraform-serverless"
+	s3_key = "v${var.app_version}/example.zip"
+}
+
+resource "aws_lambda_permission" "apigw" {
+	action = "lambda:InvokeFunction"
+	function_name = aws_lambda_function.example.function_name
+	principal = "apigateway.amazonaws.com"
+	source_arn = "${aws_api_gateway_rest_api.example.execution_arn}/*/*"
+	statement_id = "AllowAPIGatewayInvoke"
+}
+
+// Roles
+resource "aws_iam_role" "lambda_exec" {
+	name = "serverless_example_lambda"
+	assume_role_policy = <<EOF
+{
+	"Statement": [
+		{
+			"Action": "sts:AssumeRole",
+			"Effect": "Allow",
+			"Principal": {
+				"Service": "lambda.amazonaws.com"
+			},
+			"Sid": ""
+		}
+	],
+	"Version": "2012-10-17"
+}
+	EOF
 }
